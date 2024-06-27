eda9374fe0d5071a4f1171a8bf7bd1dbe71d8ef0
mersad-b7xtrue <mersadfallah.dev@gmail.com>
2024-06-27 16:23:46 +0330

    add comment for html & js code

--- src/js/script.js
+++ src/js/script.js
@@ -2,63 +2,73 @@ const allFormats = [
     'jpg', 'png', 'gif', 'tiff', 'bmp', 'webp', 'heic', 'mp4', 'avi', 'mov', 'wmv', 'mkv', 'flv', 'webm', 'mp3', 'wav', 'flac', 'aac', 'ogg', 'm4a', 'txt', 'pdf', 'rar', 'zip', '7z', 'tar.gz', 'docx', 'html', 'csv', 'srt', 'svg', 'xls', 'xlsx', 'pptx', 'json', 'xml', 'md', 'epub', 'mobi', 'odt'
 ];
 
-const aboutBtn = document.querySelector(".about")
+// DOM Elements
+const aboutBtn = document.querySelector(".about");
 const formatBtn = document.querySelector(".format-btn");
 const buttonArrow = document.querySelector(".button-arrow");
-const formatedlist = document.querySelector(".format-lists");
+const formatList = document.querySelector(".format-lists");
 const inputText = document.querySelector('.app-input');
 const outputDiv = document.querySelector('.app-output');
-const deleteLinks = document.querySelector(".delete");
+const deleteLinksBtn = document.querySelector(".delete");
 const copyMessage = document.querySelector(".copied");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const closeModalBtn = document.querySelector(".close-modal");
 const loader = document.querySelector(".loader");
 
-
 let links = [];
 let format = "";
 
+// Function to handle format item click
 function formatItemClickHandler(event) {
+    // Reset background color for all format items
     document.querySelectorAll(".format-items").forEach((li) => {
         li.style.backgroundColor = "";
     });
 
-    this.style.backgroundColor = "#007bff21";
-    format = this.textContent;
-    document.querySelector(".format-type").textContent = `(${format})`
-    formatedlist.classList.add("hidden");
+    // Set background color for selected format item
+    event.currentTarget.style.backgroundColor = "#007bff21";
+    format = event.currentTarget.textContent;
+    document.querySelector(".format-type").textContent = format;
+    formatList.classList.add("hidden");
     buttonArrow.classList.remove("-rotate-90");
 }
 
+// Function to show a sweet alert with an error message
 const sweetAlert = (errorMessage) => {
     Swal.fire({
         title: errorMessage,
         icon: 'error',
         confirmButtonText: 'بازگشت'
-    })
-    return
+    });
 };
 
+// Function to find and display links with the selected format
 function findLink() {
     const linksArray = inputText.value.split('\n');
     outputDiv.innerHTML = '';
     outputDiv.classList.remove("hidden");
 
-    if (inputText.value === "") {
+    // Check if input is empty or format is not selected
+    if (!inputText.value) {
         sweetAlert('شما لینکی وارد نکرده اید');
         return;
-    } else if (format === "") {
+    }
+    if (!format) {
         sweetAlert('لطفا فرمت مورد نظر را انتخاب کنید');
         return;
     }
 
+    // Show loader
     loader.classList.remove("hidden");
     loader.classList.add("flex");
+
+    // Simulate loading with setTimeout
     setTimeout(() => {
         loader.classList.add("hidden");
         loader.classList.remove("flex");
 
+        // Iterate over input links and display links with the selected format
         linksArray.forEach(link => {
             if (link.endsWith(`.${format}`)) {
                 const html = `
@@ -70,25 +80,26 @@ function findLink() {
             }
         });
 
-        if (outputDiv.innerHTML === "") {
-            outputDiv.insertAdjacentHTML("beforeend", `<p dir="rtl" class="py-4">فرمت مورد نظر در لینک ها وجود ندارد :(</p>`);
+        // Show message if no links with the selected format are found
+        if (!outputDiv.innerHTML) {
+            outputDiv.insertAdjacentHTML("beforeend", '<p dir="rtl" class="py-4">فرمت مورد نظر در لینک ها وجود ندارد :(</p>');
         }
     }, 1000);
 }
 
+// Function to copy all displayed links to the clipboard
 function copyLinks() {
     copyMessage.classList.remove("hidden");
-    document.querySelectorAll(".link-box").forEach(linkBox => {
-        const link = linkBox.querySelector("a").href;
-        links.push(link);
-    })
+    links = Array.from(document.querySelectorAll(".link-box a")).map(link => link.href);
     navigator.clipboard.writeText(links.join("\n"));
 
+    // Hide copy message after 3 seconds
     setTimeout(() => {
         copyMessage.classList.add("hidden");
     }, 3000);
 }
 
+// Function to close the modal
 const closeModal = () => {
     modal.classList.add("translate-x-full");
     modal.classList.remove("-translate-x-1/2");
@@ -97,31 +108,34 @@ const closeModal = () => {
         modal.classList.add("hidden");
         overlay.classList.add("hidden");
     }, 500);
-}
+};
 
+// Event listener to populate format list on window load
 window.addEventListener("load", () => {
-    allFormats.sort();
-    allFormats.forEach(format => {
+    allFormats.sort().forEach(format => {
         const li = `<li class="format-items cursor-pointer py-3 px-3 border-b hover:bg-blue-400/30 border-y-gray-300 text-zinc-700">${format}</li>`;
-        formatedlist.insertAdjacentHTML("beforeend", li);
-    })
-})
+        formatList.insertAdjacentHTML("beforeend", li);
+    });
+});
 
+// Event listener to show/hide format list and add click handlers to format items
 formatBtn.addEventListener("click", () => {
-    formatedlist.classList.toggle("hidden");
+    formatList.classList.toggle("hidden");
     buttonArrow.classList.toggle("-rotate-90");
 
-    document.querySelectorAll(".format-items").forEach((li) => {
+    document.querySelectorAll(".format-items").forEach(li => {
         li.removeEventListener("click", formatItemClickHandler);
         li.addEventListener("click", formatItemClickHandler);
     });
 });
 
-deleteLinks.addEventListener("click", () => {
+// Event listener to delete all displayed links
+deleteLinksBtn.addEventListener("click", () => {
     outputDiv.innerHTML = '';
     links = [];
 });
 
+// Event listener to open the modal
 aboutBtn.addEventListener("click", () => {
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
@@ -132,12 +146,12 @@ aboutBtn.addEventListener("click", () => {
     }, 0.1);
 });
 
-closeModalBtn.addEventListener("click", () => {
-    closeModal()
-});
+// Event listener to close the modal
+closeModalBtn.addEventListener("click", closeModal);
 
+// Event listener to close the modal on Escape key press
 document.addEventListener("keyup", (event) => {
     if (event.key === "Escape") {
         closeModal();
     }
-})
+});
